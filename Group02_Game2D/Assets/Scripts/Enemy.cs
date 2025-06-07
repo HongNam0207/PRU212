119003a443e13a2fe8f67df57156508391e03348
Nguyễn Hồng Nam <hongnam0207@gmail.com>
2025-06-07 10:56:54 +0700

    Fix Basic Enemy

--- Group02_Game2D/Assets/Scripts/Enemy.cs
+++ Group02_Game2D/Assets/Scripts/Enemy.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
-
+using UnityEngine.UI;
 public abstract class Enemy : MonoBehaviour
 {
     [SerializeField] protected float enemyMoveSpeed = 1f;
     protected Player player;
+    [SerializeField] protected float maxHp = 50f;
+    protected float currentHp;
+    [SerializeField] private Image hpBar;
+    [SerializeField] protected float enterDamage = 10f;
+    [SerializeField] protected float stayDamage = 1f;
 
     protected virtual void Start()
     {
         player = FindAnyObjectByType<Player>();
+        currentHp = maxHp;
+        UpdateHpBar();
     }
     protected virtual void Update()
     {
@@ -38,4 +45,26 @@ public abstract class Enemy : MonoBehaviour
             );
         }
     }
+    public virtual void TakeDamage(float damage)
+    {
+        currentHp -= damage;
+        currentHp = Mathf.Max(currentHp, 0);
+        UpdateHpBar();
+        if (currentHp <= 0)
+        {
+            Die();
+        }
+    }
+    protected virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+    protected void UpdateHpBar()
+    {
+        if (hpBar != null)
+        {
+            hpBar.fillAmount = currentHp / maxHp ;
+        }
+    }
+
 }
