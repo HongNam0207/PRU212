7c6a17bda77347786d1e7b280bf57b5c1aefd2cb
Nguyễn Hồng Nam <hongnam0207@gmail.com>
2025-07-02 23:35:31 +0700

    Create Energy Bar

---
+++ Group02_Game2D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int currentEnergy;
+    [SerializeField] private int energyThreshold = 3;
+    [SerializeField] private GameObject boss;
+    [SerializeField] private GameObject enemySpaner;
+    private bool bossCalled = false;
+    [SerializeField] private Image energyBar;
+    [SerializeField] GameObject gameUi;
+
+    void Start()
+    {
+        currentEnergy = 0;
+        UpdateEnergyBar();
+        boss.SetActive(false);
+
+    }
+
+
+    void Update()
+    {
+        // Logic kiểm tra win/trigger ở đây
+    }
+
+    public void AddEnergy()
+    {
+        if (bossCalled)
+        {
+            return;
+        }
+
+        currentEnergy += 1;
+        UpdateEnergyBar();
+        if (currentEnergy == energyThreshold)
+        {
+            CallBoss();
+        }
+    }
+
+
+    private void CallBoss()
+    {
+        bossCalled = true;
+        boss.SetActive(true);
+        enemySpaner.SetActive(false);
+        gameUi.SetActive(false);
+
+    }
+
+    private void UpdateEnergyBar()
+    {
+        if (energyBar != null)
+        {
+            float fillAmount = Mathf.Clamp01((float)currentEnergy / (float)energyThreshold);
+            energyBar.fillAmount = fillAmount;
+        }
+    }
+
+
+}
